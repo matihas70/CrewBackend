ac6f82b7e68bd3e964d6aa7a2e2f48c69f055492
matihas <matihasek@gmail.com>
2024-02-11 17:37:45 +0100

    add groups controller and service

--- Controllers/GroupsController.cs
+++ Controllers/GroupsController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Core;
+using CrewBackend.Interfaces;
+using CrewBackend.Models;
+using CrewBackend.Models.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace CrewBackend.Controllers
 {
+    [ApiController]
+    [Authorize]
+    [Route("Group")]
     public class GroupsController : Controller
     {
-        public IActionResult Index()
+        private readonly IUserContextInfo userContextInfo;
+        private readonly IGroupsService groupsService;
+        public GroupsController(IUserContextInfo _userContextInfo, IGroupsService _groupsService) =>
+           (userContextInfo, groupsService) = (_userContextInfo, _groupsService);
+        
+        
+        [HttpPost]
+        public IActionResult CreateGroup([FromBody]CreateGroupDto dto)
         {
-            return View();
+            long userId = userContextInfo.GetUserId();
+            ResponseModel<object> response = groupsService.CreateGroup(dto, userId);
+            if(response.Status == Data.Enums.StatusEnum.ResourceExist)
+            {
+                return BadRequest(response.Message);
+            }
+            return Created();
         }
     }
 }
---
+++ Services/GroupsService.cs
@@ -0,0 +1,51 @@
+﻿using CrewBackend.Entities;
+using CrewBackend.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using CrewBackend.Models;
+using CrewBackend.Models.Dto;
+namespace CrewBackend.Services
+{
+    public class GroupsService:IGroupsService
+    {
+        private readonly IDbContextFactory<CrewDbContext> dbFactory;
+        public GroupsService(IDbContextFactory<CrewDbContext> _dbFactory) =>
+            dbFactory = _dbFactory;
+
+        public ResponseModel<object> CreateGroup(CreateGroupDto dto, long userId)
+        {
+            ResponseModel<object> response = new ResponseModel<object>();
+
+            using CrewDbContext db = dbFactory.CreateDbContext();
+
+            if(db.Groups.Any(g => g.Name == dto.Name))
+            {
+                response.Status = Data.Enums.StatusEnum.ResourceExist;
+                response.Message = "Group name already exist";
+                return response;
+            }
+
+            Group newGroup = new Group
+            {
+                Name = dto.Name,
+                CreatedBy = userId,
+                CreateDate = DateTime.Now
+            };
+
+            UsersGroup newUserGroup = new UsersGroup
+            {
+                UserId = userId,
+                Group = newGroup,
+                RoleId = (int)Data.Enums.Roles.Admin
+            };
+
+            db.Groups.Add(newGroup);
+            db.UsersGroups.Add(newUserGroup);
+            db.SaveChanges();
+
+            response.Status = Data.Enums.StatusEnum.Ok;
+            return response;
+
+        }
+
+    }
+}
